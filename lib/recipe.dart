e32806d90ce6ef4c61f92ffe7fced98e88dd27b5
mswift42 <martin.haesler@gmail.com>
2021-08-23 09:43:22 +0100

    use lowerCamelCase for const names.

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -108,8 +108,8 @@ class SearchQuery {
   SearchQuery(this.searchterm, this.page, this.searchFilter);
 }
 
-const CKPrefix = 'www.chefkoch.de';
-const BBGFPrefix = 'www.bbcgoodfood.com/search';
+const cKPrefix = 'www.chefkoch.de';
+const bBGFPrefix = 'www.bbcgoodfood.com/search';
 
 class RecDocument {
   String searchterm;
@@ -124,7 +124,7 @@ class RecDocument {
   }
 
   Uri queryUrl() {
-    var addr = '$CKPrefix/rs/s$page$searchfilter/$searchterm/Rezepte.html';
+    var addr = '$cKPrefix/rs/s$page$searchfilter/$searchterm/Rezepte.html';
     return Uri.https(addr, '');
   }
 
@@ -154,7 +154,7 @@ class BGFDocument extends RecDocument {
   @override
   Uri queryUrl() {
     var addr =
-        '$BBGFPrefix/recipes?query=$searchterm&page=$page${searchfilter != "" ? "&sort=" + searchfilter : ""}';
+        '$bBGFPrefix/recipes?query=$searchterm&page=$page${searchfilter != "" ? "&sort=" + searchfilter : ""}';
     return Uri.https(addr, '');
   }
 }
